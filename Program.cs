38ecbcc45d073dd99f96ad55ff45dcc52d0a50ee
Ana Carolina <anacarola459@gmail.com>
2024-01-06 02:54:16 -0300

    Teste

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Insira um número:");
+
+int numeroUm =Convert.ToInt32(Console.ReadLine());
+int numeroDois =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Resultado:"+(numeroUm + numeroDois)); 
